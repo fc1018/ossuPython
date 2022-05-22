d434b70cddbd55bbb2734db13053e789f4883938
Felix <fc1018@ic.ac.uk>
2022-05-22 12:04:52 +0100

    Added try and except

--- Chapter_3/ch3_ex2.py
+++ Chapter_3/ch3_ex2.py
@@ -1,5 +1,10 @@
-hrs = float(input("Enter hours: "))
-rate = float(input("Enter rate: "))
+try:
+    hrs = float(input("Enter hours: "))
+    rate = float(input("Enter rate: "))
+except: 
+    print("Error, please enter numeric input ")
+    quit()
+    
 if hrs > 40 :
     pay = ((hrs - 40) * (rate * 1.5)) + (40 * rate)
 else :
